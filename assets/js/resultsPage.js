5a907b57c50991f0ba2731906fc2eb3780d28f31
Yan Ashe <yanashe@Yans-Mac-Studio.local>
2024-02-08 21:53:02 -0600

    edit the lyrics

--- assets/js/resultsPage.js
+++ assets/js/resultsPage.js
@@ -1,14 +1,26 @@
-let lyr = document.getElementById("lyr")
+let lyr = document.getElementById("lyr");
 var requestOptions = {
     method: 'GET',
     redirect: 'follow'
-  };
-  
-  fetch("https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime", requestOptions)
-    .then(response => response.text())
-    .then(result => lyr.textContent= result)
+};
+
+fetch("https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime", requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        let lyrics = result.lyrics;
+        
+        // Split the lyrics by line breaks
+        const lines = lyrics.split('\n');
+        
+        // Remove the first line
+        lines.shift();
+        
+        // Join the remaining lines back together
+        const modifiedLyrics = lines.join('\n').trim();
+        
+        // Format the modified lyrics with <br> tags
+        const formattedText = modifiedLyrics.replace(/\n/g, '<br>');
+        
+        lyr.innerHTML = formattedText;
+    })
     .catch(error => console.log('error', error));
-
-
-
-    
